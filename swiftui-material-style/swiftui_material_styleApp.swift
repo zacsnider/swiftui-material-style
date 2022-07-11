5ff8622eec2847f5b5c6803e42da332bda710818
Zac Snider <zdsnider@gmail.com>
2022-07-11 17:41:19 -0400

    Initial Commit

---
+++ swiftui-material-style/swiftui_material_styleApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_material_styleApp.swift
+//  swiftui-material-style
+//
+//  Created by Zac Snider on 7/11/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_material_styleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
